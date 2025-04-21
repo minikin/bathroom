a61da0d338df748ec312ff1cf823216beab7d217
Mr-Leshiy <leshiy12345678@gmail.com>
2025-04-21 19:23:07 +0900

    fix fmt

--- benches/bathroom.rs
+++ benches/bathroom.rs
@@ -7,40 +7,36 @@
 use std::collections::HashMap;
 
 use bathroom::BathroomMap;
-use criterion::{criterion_group, criterion_main, Criterion};
-use proptest::{ prelude::{ any, Strategy}, strategy::ValueTree, test_runner::TestRunner};
+use criterion::{Criterion, criterion_group, criterion_main};
+use proptest::{
+    prelude::{Strategy, any},
+    strategy::ValueTree,
+    test_runner::TestRunner,
+};
 
 const ITEMS_AMOUNT: usize = 1000;
 const SAMPLE_SIZE: usize = 10;
 
 fn hash_map_benches(c: &mut Criterion) {
     let mut runner = TestRunner::default();
-    let items = any::<[(String, String); ITEMS_AMOUNT]>()
-    .new_tree(&mut runner)
-    .unwrap()
-    .current();
-
+    let items = any::<[(String, String); ITEMS_AMOUNT]>().new_tree(&mut runner).unwrap().current();
 
     let mut group = c.benchmark_group("Hash map comparison benchmark");
     group.sample_size(SAMPLE_SIZE);
     let mut bathroom_map = BathroomMap::new();
     let mut rust_map = HashMap::new();
     group.bench_function("bathroom insert", |b| {
-        b.iter(
-            || {
+        b.iter(|| {
             for (key, value) in items.clone() {
                 bathroom_map.insert(key, value);
             }
-            
         });
     });
     group.bench_function("rust std insert", |b| {
-        b.iter(
-            || {
+        b.iter(|| {
             for (key, value) in items.clone() {
                 rust_map.insert(key, value);
             }
-            
         });
     });
     group.bench_function("bathroom get", |b| {
